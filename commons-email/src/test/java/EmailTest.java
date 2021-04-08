dbced3bdd130d6bb062bf18c565042051e634c87
aminahehsan <aminahehsan@192.168.4.33>
2021-04-08 18:36:33 -0400

    setUpEmail and tearDownEmail tests

--- commons-email/src/test/java/EmailTest.java
+++ commons-email/src/test/java/EmailTest.java
@@ -1,4 +1,27 @@
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
 
 public class EmailTest {
+	
+	private static final String[] TEST_EMAILS = { "ab@bc.com", "a.b@c.org",
+	"acbdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd"};
 
+private EmailConcrete email;
+private Date d = new Date(2021-25-4);
+private int connectionS = 3;
+private static final String emailName = "abc@gmail.com";
+private static final String value = "eVal";
+
+
+@Before
+public void setUpEmailTest() throws Exception{
+email = new EmailConcrete();
+}
+
+@After
+public void tearDownEmailTest() throws Exception{
+
+}
 }
